0126f363a5a70d2dc09172edacdb019228e6fac2
wyman <voyagingmk@gmail.com>
2018-02-05 12:52:51 +0800

    no message

--- Makefile
+++ Makefile
@@ -1,39 +1,40 @@
 
 all: example_client example_server
 
+
 OBJS = ae.o zmalloc.o ikcp.o error.o wrapsock.o common.o
 
 example_client : example_client.o ${OBJS}
-	g++  -o example_client  example_client.o ${OBJS}
+	g++ -g3 -o example_client  example_client.o ${OBJS}
 
 example_server : example_server.o ${OBJS}
-	g++  -o example_server example_server.o ${OBJS}
+	g++ -g3 -o example_server example_server.o ${OBJS}
 
 example_client.o : example_client.cpp
-	g++ -c example_client.cpp
+	g++ -g3 -c example_client.cpp
 
 example_server.o : example_server.cpp
-	g++ -c example_server.cpp
+	g++ -g3 -c example_server.cpp
 
 
 common.o : common.cpp 
-	g++ -c common.cpp
+	g++ -g3 -c common.cpp
 
 
 ae.o : ae.c
-	g++ -x c -c ae.c
+	g++ -g3 -x c -c ae.c
 
 error.o : error.c
-	g++ -x c -c error.c
+	g++ -g3 -x c -c error.c
 
 wrapsock.o : wrapsock.c
-	g++ -x c -c wrapsock.c
+	g++ -g3 -x c -c wrapsock.c
 
 zmalloc.o : zmalloc.c
-	g++ -x c -c zmalloc.c
+	g++ -g3 -x c -c zmalloc.c
 
 ikcp.o : ikcp.c
-	g++ -x c -c ikcp.c
+	g++ -g3 -x c -c ikcp.c
 	
 .PHONY: clean
 
--- example_server.cpp
+++ example_server.cpp
@@ -1,46 +1,35 @@
 #include "common.h"
 
 #define MAX_MSG 100
-int main (int argc, char **argv) {
+void CustomFileProc(struct aeEventLoop *eventLoop,
+                    int fd, void *clientData, int mask)
+{
+    UDPServer *server = (UDPServer *)(clientData);
+    char msg[MAX_MSG];
+    memset(msg, 0x0, MAX_MSG);
+    struct sockaddr_in cliAddr;
+    socklen_t len = sizeof(cliAddr);
+    Recvfrom(server->m_sockfd, msg, MAX_MSG, 0,
+             (struct sockaddr *)&cliAddr, &len);
+
+    printf("recv from UDP %s:%u : %s \n",
+           inet_ntoa(cliAddr.sin_addr),
+           ntohs(cliAddr.sin_port),
+           msg);
+}
+
+int main(int argc, char **argv)
+{
     signal(SIGPIPE, SIG_IGN);
     UDPServer server(Port);
-    
-	fd_set rset;
-    struct sockaddr_in cliAddr;
-    char msg[MAX_MSG];
-    FD_ZERO(&rset);
-	int maxfd = server.m_sockfd + 1;
-    int nready;
-    while(1) {
-		FD_SET(server.m_sockfd, &rset);
-        if ( (nready = select(maxfd, &rset, NULL, NULL, NULL)) < 0) {
-			if (errno == EINTR)
-				continue;		/* back to for() */
-			else
-				err_sys("select error");
-		}
-        if (FD_ISSET(server.m_sockfd, &rset)) {
-            /* init buffer */
-            memset(msg, 0x0, MAX_MSG);
-            /* receive message */
-            socklen_t len = sizeof(cliAddr);
-            Recvfrom(server.m_sockfd, msg, MAX_MSG, 0, 
-                (struct sockaddr *) &cliAddr, &len);
-            
-            /* print received message */
-            printf("recv from UDP %s:%u : %s \n", 
-                inet_ntoa(cliAddr.sin_addr),
-                ntohs(cliAddr.sin_port),
-                msg);
-        }
-        
-    }
     /*
     KCPObject kcpObject(Conv, Interval);
     kcpObject.bindSocket(&server);
-
+    */
+    printf("aeGetApiName: %s\n", aeGetApiName());
     loop = aeCreateEventLoop(64);
-    aeMain(loop);*/
+    aeCreateFileEvent(loop, server.m_sockfd, AE_READABLE,
+                      CustomFileProc, &server);
+    aeMain(loop);
     return 0;
 }
-
