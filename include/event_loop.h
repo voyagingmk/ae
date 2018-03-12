061b0c36546922196f4e49145aac432249e74677
wyman <voyagingmk@gmail.com>
2018-03-12 14:33:30 +0800

    no message

--- include/event_loop.h
+++ include/event_loop.h
@@ -4,7 +4,6 @@
 #include "noncopyable.h"
 #include "common.h"
 
-
 namespace wynet
 {
 
@@ -15,48 +14,48 @@ class EventLoop : Noncopyable
     typedef int (*OnTimerEvent)(EventLoop *, int timerfd, void *userData);
 
     EventLoop(int defaultSetsize = 64);
-    
+
     ~EventLoop();
 
     void loop();
 
     void stop();
-    
+
     void createFileEvent(int fd, int mask, OnFileEvent onFileEvent, void *userData);
 
     void deleteFileEvent(int fd, int mask);
-    
-    long long createTimerEvent(long long ms, OnTimerEvent onTimerEvent, void *userData);
 
-public:
-    
+    long long createTimer(long long ms, OnTimerEvent onTimerEvent, void *userData);
+
+    bool deleteTimer(long long timerid);
+
+  public:
     struct FDData
     {
-        FDData():
-            onFileEvent(nullptr),
-            userDataRead(nullptr),
-            userDataWrite(nullptr)
-        {}
+        FDData() : onFileEvent(nullptr),
+                   userDataRead(nullptr),
+                   userDataWrite(nullptr)
+        {
+        }
         OnFileEvent onFileEvent;
         void *userDataRead;
         void *userDataWrite;
     };
-    
+
     struct TimerData
     {
-        TimerData():
-            onTimerEvent(nullptr),
-            userData(nullptr)
-        {}
+        TimerData() : onTimerEvent(nullptr),
+                      userData(nullptr)
+        {
+        }
         OnTimerEvent onTimerEvent;
         void *userData;
     };
-    
 
-private:
+  private:
     friend void aeOnFileEvent(struct aeEventLoop *eventLoop, int fd, void *clientData, int mask);
     friend int aeOnTimerEvent(struct aeEventLoop *eventLoop, long long timerid, void *clientData);
-    
+
     aeEventLoop *aeloop;
     std::map<int, std::shared_ptr<FDData>> fdData;
     std::map<long long, std::shared_ptr<TimerData>> timerData;
--- src/event_loop.cpp
+++ src/event_loop.cpp
@@ -5,27 +5,33 @@ namespace wynet
 
 void aeOnFileEvent(struct aeEventLoop *eventLoop, int fd, void *clientData, int mask)
 {
-    EventLoop* loop = (EventLoop*)(clientData);
+    EventLoop *loop = (EventLoop *)(clientData);
     std::shared_ptr<EventLoop::FDData> p = loop->fdData[fd];
-    if (p && p->onFileEvent){
-        if (mask & AE_READABLE) {
+    if (p && p->onFileEvent)
+    {
+        if (mask & AE_READABLE)
+        {
             p->onFileEvent(loop, fd, p->userDataRead, mask);
         }
-        if (mask & AE_WRITABLE) {
+        if (mask & AE_WRITABLE)
+        {
             p->onFileEvent(loop, fd, p->userDataWrite, mask);
         }
     }
 }
-    
-int aeOnTimerEvent(struct aeEventLoop *eventLoop, long long timerid, void *clientData) {
-    EventLoop* loop = (EventLoop*)(clientData);
+
+int aeOnTimerEvent(struct aeEventLoop *eventLoop, long long timerid, void *clientData)
+{
+    EventLoop *loop = (EventLoop *)(clientData);
     std::shared_ptr<EventLoop::TimerData> p = loop->timerData[timerid];
-    if (!p || !p->onTimerEvent) {
+    if (!p || !p->onTimerEvent)
+    {
         return AE_NOMORE;
     }
     int ret = p->onTimerEvent(loop, timerid, p->userData);
-    if (ret == AE_NOMORE) {
-       loop->timerData.erase(timerid);
+    if (ret == AE_NOMORE)
+    {
+        loop->timerData.erase(timerid);
     }
     return ret;
 }
@@ -58,42 +64,53 @@ void EventLoop::stop()
 
 void EventLoop ::createFileEvent(int fd, int mask, OnFileEvent onFileEvent, void *userData)
 {
-    int setsize = aeGetSetSize(aeloop) ;
-    while (fd >= setsize) {
+    int setsize = aeGetSetSize(aeloop);
+    while (fd >= setsize)
+    {
         assert(AE_ERR != aeResizeSetSize(aeloop, setsize << 1));
     }
     int ret = aeCreateFileEvent(aeloop, fd, mask, aeOnFileEvent, (void *)this);
     assert(AE_ERR != ret);
-    if (fdData.find(fd) == fdData.end()) {
+    if (fdData.find(fd) == fdData.end())
+    {
         std::shared_ptr<FDData> p(new FDData());
         fdData.insert({fd, p});
     }
     std::shared_ptr<FDData> p = fdData[fd];
     p->onFileEvent = onFileEvent;
-    if (mask & AE_READABLE) {
+    if (mask & AE_READABLE)
+    {
         p->userDataRead = userData;
     }
-    if (mask & AE_WRITABLE) {
+    if (mask & AE_WRITABLE)
+    {
         p->userDataWrite = userData;
     }
-    
 }
 
 void EventLoop ::deleteFileEvent(int fd, int mask)
 {
     aeDeleteFileEvent(aeloop, fd, mask);
 }
-    
-long long EventLoop ::createTimerEvent(long long ms, OnTimerEvent onTimerEvent, void *userData)
+
+long long EventLoop ::createTimer(long long ms, OnTimerEvent onTimerEvent, void *userData)
 {
     long long timerid = aeCreateTimeEvent(aeloop, ms, aeOnTimerEvent, (void *)this, NULL);
     assert(AE_ERR != timerid);
-    if (timerData.find(timerid) == timerData.end()) {
+    if (timerData.find(timerid) == timerData.end())
+    {
         std::shared_ptr<TimerData> p(new TimerData());
         timerData.insert({timerid, p});
     }
     std::shared_ptr<TimerData> p = timerData[timerid];
     p->onTimerEvent = onTimerEvent;
     return timerid;
+}
+
+bool EventLoop ::deleteTimer(long long timerid)
+{
+    timerData.erase({timerid});
+    int ret = aeDeleteTimeEvent(aeloop, timerid);
+    return AE_ERR != ret;
 }
 };
