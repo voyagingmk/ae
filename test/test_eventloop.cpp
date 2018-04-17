33ef9b74ccb358b8934d5914ffac1fe0386bb26a
wyman <voyagingmk@gmail.com>
2018-04-17 16:22:36 +0800

    no message

--- test/test_eventloop.cpp
+++ test/test_eventloop.cpp
@@ -1,9 +1,26 @@
 #include "wynet.h"
 using namespace wynet;
 
+void threadFunc()
+{
+    printf("threadFunc(): pid = %d, tid = %d\n", getpid(), CurrentThread::tid());
+    printf("threadFunc(): isMainThread: %s\n", CurrentThread::isMainThread() ? "true" : "false");
+
+    EventLoop loop;
+    loop.loop();
+}
+
 int main(int argc, char **argv)
 {
     printf("main(): pid = %d, tid = %d\n", getpid(), CurrentThread::tid());
     printf("main(): isMainThread: %s\n", CurrentThread::isMainThread() ? "true" : "false");
+
+    Thread thread(threadFunc);
+    thread.start();
+
+    EventLoop loop;
+    loop.loop();
+
+    pthread_exit(NULL);
     return 0;
 }
