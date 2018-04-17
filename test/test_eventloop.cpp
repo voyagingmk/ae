b674bdf3cf93e569a132858fc837c793ac530da0
wyman <voyagingmk@gmail.com>
2018-04-17 15:31:40 +0800

    no message

--- test/test_eventloop.cpp
+++ test/test_eventloop.cpp
@@ -1,5 +1,9 @@
+#include "wynet.h"
+using namespace wynet;
 
 int main(int argc, char **argv)
 {
+    printf("main(): pid = %d, tid = %d\n", getpid(), CurrentThread::tid());
+    printf("main(): isMainThread: %s\n", CurrentThread::isMainThread() ? "true" : "false");
     return 0;
 }
