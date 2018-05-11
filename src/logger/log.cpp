bffc5105f8570baf4eac5361970575459b29baf4
wyman <voyagingmk@gmail.com>
2018-05-11 12:54:11 +0800

    no message

--- src/logger/log.cpp
+++ src/logger/log.cpp
@@ -97,7 +97,7 @@ void log_log(LOG_LEVEL level, const char *file, int line, const char *fmt, ...)
     {
         //  4 +    2 +    2 +   1 +     3 +     3 +     3 +     7 +   1 +  8 +   1 +     5 = 40
         //%4d + %02d + %02d + ' ' + %02d: + %02d: + %02d. + %06dZ + ' ' + %s + ' ' + level
-        n = snprintf(buff, prefixN, "%4d%02d%02d %02d:%02d:%02d.%06lZ %s %s",
+        n = snprintf(buff, prefixN, "%4d%02d%02d %02d:%02d:%02d.%06ldZ %s %s",
                      tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
                      tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec, tv.tv_usec,
                      CurrentThread::tidString(),
