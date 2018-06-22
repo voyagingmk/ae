97eceffe97e4965a00ee16ef136b4567366e6f95
wyman <voyagingmk@gmail.com>
2018-06-22 18:26:37 +0800

    no message

--- src/eventloop.cpp
+++ src/eventloop.cpp
@@ -341,7 +341,7 @@ void EventLoop ::deleteFileEventInLoop(SockFd sockfd, int mask)
     }*/
     aeDeleteFileEvent(m_aeloop, sockfd, mask);
     //  int ret = m_mploop.deleteFileEvent(sockfd, mask);
-    assert(AE_ERR != ret);
+    //   assert(AE_ERR != ret);
     if (!getFileEvent(sockfd) && m_fd2listener.find(sockfd) != m_fd2listener.end())
     {
         m_fd2listener.erase(sockfd);
